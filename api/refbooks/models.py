a676468912661b941a5eb2c40f60834802952d87
Roman Poliakov <roman61394@gmail.com>
2022-12-11 15:16:40 +0400

    add custom restrictions

--- api/refbooks/models.py
+++ api/refbooks/models.py
@@ -5,7 +5,7 @@ class ReferenceBook(models.Model):
     code = models.CharField(max_length=100, unique=True,
                             null=False, blank=False)
     name = models.CharField(max_length=300, null=False, blank=False)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.code
@@ -21,24 +21,32 @@ class ReferenceBookVersion(models.Model):
         constraints = [
             models.UniqueConstraint(
                 fields=['reference_book', 'version'],
-                name='unique_version'
+                name='unique_refbook_and_version'
             ),
             models.UniqueConstraint(
                 fields=['reference_book', 'start_date'],
-                name='unique_date'
+                name='unique_refbook_and_date'
             )
         ]
 
     def __str__(self):
-        return self.version
+        return f'{self.reference_book.code} ({self.version})'
 
 
 class ReferenceBookItem(models.Model):
     reference_book_version = models.ForeignKey(ReferenceBookVersion,
                                                on_delete=models.CASCADE)
-    code = models.CharField(max_length=100, unique=True,
-                            null=False, blank=False)
+    code = models.CharField(max_length=100, null=False, blank=False)
     value = models.CharField(max_length=300, null=False, blank=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['reference_book_version',
+                        'code'],
+                name='unique_version_and_code'
+            )
+        ]
 
     def __str__(self):
         return self.code
